0a416616c609ff429777a9ad299a6b9abbac0cce
Yosef64 <yosefale65@gmail.com>
2024-11-20 11:32:04 +0300

    allowing all request

--- api/index.py
+++ api/index.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI, Request, Response
 from .dbActions import getSession,registerAgent
 from fastapi.middleware.cors import CORSMiddleware
-origins = [
-    "https://victory-contest.vercel.app/",
-]
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+
+# origins = [
+#     "https://victory-contest.vercel.app/",
+# ]
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True,  
+    allow_origins=["*"],   
     allow_methods=["*"],  
     allow_headers=["*"],
 )
-
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    print(f"Request: {request.method} {request.url}")
+    response = await call_next(request)
+    print(f"Response: {response.status_code}")
+    return response
 @app.post("/register")
 async def agentRegister(request:Request):
     
