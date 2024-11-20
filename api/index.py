603e408853a1ff3abb51ad79c84c0f3e7f729ed7
Yosef64 <yosefale65@gmail.com>
2024-11-20 11:08:09 +0300

    removing credentials

--- api/index.py
+++ api/index.py
@@ -8,10 +8,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, 
-    allow_credentials=True,  
+    allow_credentials=False,  
     allow_methods=["*"],  
     allow_headers=["*"],
 )
+
 @app.post("/register")
 async def agentRegister(request:Request):
     
